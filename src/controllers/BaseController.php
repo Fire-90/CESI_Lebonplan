c6da6c0ea6117cb443ba37be34fead3011f308fd
Baptiste-01 <baptiste.morin1@viacesi.fr>
2025-03-27 11:06:48 +0100

    Home + Controller

---
+++ src/controllers/BaseController.php
@@ -0,0 +1,11 @@
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+class BaseController {
+    protected $twig;
+
+    public function __construct() {
+        $loader = new FilesystemLoader(__DIR__ . '/../views');
+        $this->twig = new Environment($loader);
+    }
+}
---
+++ src/controllers/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once __DIR__ . '/../core/TemplateEngine.php';
+
+class HomeController {
+    public function home() {
+        echo TemplateEngine::render('home.twig', ['current_page' => 'home']);
+    }
+
+    public function entreprises() {
+        echo TemplateEngine::render('entreprises.twig', ['current_page' => 'entreprises']);
+    }
+
+    public function offres() {
+        echo TemplateEngine::render('offres.twig', ['current_page' => 'offres']);
+    }
+
+    public function whishlist() {
+        echo TemplateEngine::render('whishlist.tiwg', ['current_page' => 'whishlist']);
+    }
+
+    public function contact() {
+        echo TemplateEngine::render('contact.twig', ['current_page' => 'contact']);
+    }
+}
