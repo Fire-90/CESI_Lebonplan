30015405d36df8c9e7f458c660b128b0dfd4a93f
Nass213dz <nassouben44@gmail.com>
2025-03-28 13:45:06 +0100

    gthr

--- src/core/router.php
+++ src/core/router.php
@@ -13,32 +13,6 @@ switch ($page) {
         $controller = new EntrepriseController();
         $controller->index();
         break;
-    case 'ajouter-entreprise':  // Ajouter la route pour l'ajout d'une entreprise
-        $controller = new EntrepriseController();
-        $controller->add();
-        break;
-    case 'modifier-entreprise':  // Ajouter la route pour la modification d'une entreprise
-        // Vous devez également récupérer l'ID de l'entreprise à modifier
-        $id = $_GET['id'] ?? null;
-        if ($id) {
-            $controller = new EntrepriseController();
-            $controller->edit($id);
-        } else {
-            // Gérer le cas où l'ID n'est pas fourni
-            echo "ID de l'entreprise manquant.";
-        }
-        break;
-    case 'supprimer-entreprise':  // Ajouter la route pour la suppression d'une entreprise
-        // Vous devez également récupérer l'ID de l'entreprise à supprimer
-        $id = $_GET['id'] ?? null;
-        if ($id) {
-            $controller = new EntrepriseController();
-            $controller->delete($id);
-        } else {
-            // Gérer le cas où l'ID n'est pas fourni
-            echo "ID de l'entreprise manquant.";
-        }
-        break;
     case 'offres':
         // Assurez-vous que la méthode offres() existe dans le bon contrôleur
         $controller = new HomeController();
