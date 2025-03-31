e368cd1f8b737decebd2a0d9d7e9b644a62e4c07
Fire-90 <louka44mine@gmail.com>
2025-03-31 11:46:55 +0200

    AAAAAAAAAAAAAA

--- src/core/database.php
+++ src/core/database.php
@@ -11,17 +11,21 @@ class Database {
     public static function getConnection() {
         if (self::$pdo === null) {
             try {
-                $host = '20.123.200.116';
-                $dbname = 'lebonplan_data'; // Vérifie bien que c'est le bon nom de la base
+                $host = 'lebonplan.westeurope.cloudapp.azure.com'; // Utilisez le nom d'hôte de votre serveur
+                $dbname = 'lebonplan_data'; // Vérifiez que c'est le bon nom de la base
                 $username = 'user';
                 $password = 'Furry_is_better666';
 
+                // Utilisation de la chaîne de connexion PDO pour se connecter à la base de données
                 self::$pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
                 self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (PDOException $e) {
-                die("Erreur de connexion : " . $e->getMessage());
+                // Gérer l'erreur de manière sécurisée sans exposer d'informations sensibles
+                error_log('Erreur de connexion à la base de données: ' . $e->getMessage());  // Enregistrer l'erreur dans les logs
+                die("Une erreur est survenue. Veuillez réessayer plus tard.");  // Message générique pour l'utilisateur
             }
         }
         return self::$pdo;
     }
 }
+
