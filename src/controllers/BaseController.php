e9eb8d47e92008f8155c6f4de3fbee0a0ca54d6b
Fire-90 <louka44mine@gmail.com>
2025-04-01 10:59:18 +0200

    Permissions

--- src/controllers/BaseController.php
+++ src/controllers/BaseController.php
@@ -7,12 +7,17 @@ use Core\TemplateEngine;
 class BaseController {
     protected $twig;
     protected $user;
+    protected $permLVL;
 
     public function __construct() {
         $this->startSession();
         $this->twig = TemplateEngine::getTwig();
         $this->user = $this->getUserFromSession();
+        $this->permLVL = $this->getPermLVLFromSession();
+        
+        // Ajout des variables globales pour Twig
         $this->twig->addGlobal('user', $this->user);
+        $this->twig->addGlobal('permLVL', $this->permLVL);
     }
 
     protected function startSession() {
@@ -25,8 +30,12 @@ class BaseController {
         return $_SESSION['user'] ?? null;
     }
 
+    protected function getPermLVLFromSession() {
+        return $_SESSION['permLVL'] ?? 0; // 0 = guest par défaut
+    }
+
     protected function render(string $template, array $data = []) {
-        $mergedData = array_merge(['user' => $this->user], $data);
-        echo $this->twig->render($template, $mergedData);
+        // Les variables user et permLVL sont déjà disponibles globalement
+        echo $this->twig->render($template, $data);
     }
 }
